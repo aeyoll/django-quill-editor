ae4f702c214878c4f27719beead48245cf79dd91
Jean-Philippe Bidegain <jp@bidega.in>
2024-09-24 09:22:58 +0200

    build: load assets localy

--- django_quill/config.py
+++ django_quill/config.py
@@ -35,23 +35,13 @@ DEFAULT_CONFIG = {
     },
 }
 MEDIA_JS = [
-    # syntax-highlight
-    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js",
-    # quill
-    "https://cdn.quilljs.com/1.3.7/quill.min.js",
-    # quill-image-compress
-    "https://cdn.jsdelivr.net/npm/quill-image-compress@1.2.21/dist/quill.imageCompressor.min.js",
-    # quill-resize
-    "https://cdn.jsdelivr.net/npm/@botom/quill-resize-module@2.0.0/dist/quill-resize-module.min.js",
-    # custom
-    "django_quill/django_quill.js",
+    'django_quill/vendor/highlight.min.js',
+    'django_quill/vendor/quill.js',
+    'django_quill/vendor/quill.imageCompressor.min.js',
+    'django_quill/django_quill.js',
 ]
 MEDIA_CSS = [
-    # syntax-highlight
-    "https://cdn.quilljs.com/1.3.7/quill.snow.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/darcula.min.css",
-    # quill-resize
-    "https://cdn.jsdelivr.net/npm/quill-resize-module@1.2.4/dist/resize.min.css",
-    # custom
-    "django_quill/django_quill.css",
+    'django_quill/vendor/quill.snow.css',
+    'django_quill/vendor/highlight.css',
+    'django_quill/django_quill.css',
 ]
